9661beefbd46203341436da5566186b0524861f9
CaminelZen <160109516+CaminelZen@users.noreply.github.com>
2024-04-09 15:30:51 +0200

    fixed nodejs issues
    added the necessary modules

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from '/Logo.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+          <a href="/index.html" target="_blank">
+          <img src={Logo} className="logo" alt="EasyBook logo" />
+          </a>
+          <h1 className="title">EasyBook - The Hotel Booking App</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div className="search-bar">
+         <input type="text" className="search-input" placeholder="Search for hotels, destinations, or properties"/>
+         <button className="search-button">Search</button>
       </div>
+      
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Search button to look for a hotel
       </p>
     </>
   )
