efb6d5c60f7473e366bf0bf88e234d39c84cf072
CaminelZen <160109516+CaminelZen@users.noreply.github.com>
2024-04-09 17:15:33 +0200

    header and search bar
    improved header and search bar

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -4,21 +4,28 @@ import './App.css'
 function App() {
   return (
     <>
+
       <div className="container">
           <a href="/index.html" target="_blank">
           <img src={Logo} className="logo" alt="EasyBook logo" />
           </a>
           <h1 className="title">EasyBook - The Hotel Booking App</h1>
       </div>
-      
+
       <div className="search-bar">
+        <div className="search-inputs">
          <input type="text" className="search-input" placeholder="Search for hotels, destinations, or properties"/>
+         <input type="text" className="date-input" placeholder="Check-in"/>
+         <input type="text" className="date-input" placeholder="Check-out"/>
+         <input type="number" className="guests-input" placeholder="Guests"/>
          <button className="search-button">Search</button>
+        </div>
       </div>
-      
+
       <p className="read-the-docs">
         Click on the Search button to look for a hotel
       </p>
+
     </>
   )
 }
