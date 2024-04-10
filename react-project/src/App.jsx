2a1e1d696677f1127e9ab9effbf768474df35269
CaminelZen <160109516+CaminelZen@users.noreply.github.com>
2024-04-10 18:22:34 +0200

    made a few adjustments
    navbar on top, gradient for the header, moved all img files in the public folder

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,28 +1,27 @@
 import Logo from '/Logo.png'
 import './App.css'
-import { openModal, closeModal, openSignupModal } from './assets/util'
-import Delft from '/src/Delft.jpg'
-import Eindhoven from '/src/Eindhoven.jpg'
-import Maastricht from '/src/Maastricht.jpg'
-import Rotterdam from '/src/Rotterdam.jpg'
-import DenHaag from '/src/DenHaag.jpg'
-import Amsterdam from '/src/Amsterdam.jpg'
+import Delft from '/Delft.jpg'
+import Eindhoven from '/Eindhoven.jpg'
+import Maastricht from '/Maastricht.jpg'
+import Rotterdam from '/Rotterdam.jpg'
+import DenHaag from '/DenHaag.jpg'
+import Amsterdam from '/Amsterdam.jpg'
 
 
 
 function App() {
   return (
     <>
-      <div className="container">
-        <a href="/index.html" target="_blank">
-          <img src={Logo} className="logo" alt="EasyBook logo" />
-        </a>
       <div className="navbar">
         <a href="#home">home</a>
         <a href="#about">about</a>
         <a href="#room">room</a>
         <a href="#review">review</a>
       </div>
+      <div className="container">
+        <a href="/index.html" target="_blank">
+          <img src={Logo} className="logo" alt="EasyBook logo" />
+        </a>
         <h1 className="title">The Hotel Booking App</h1>
       </div>
       <div className="search-bar">
@@ -38,42 +37,11 @@ function App() {
         Click on the Search button to look for a hotel
       </p>
 
-      <div id="loginModal" className="modal">
-        <div className="modal-content">
-          <span className="close" onClick="closeModal('loginModal')">&times;</span>
-          <h2>Login</h2>
-          <form>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" />
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" />
-            <button type="submit">Login</button>
-          </form>
-          <a href="#" onClick="openSignupModal()">Sign-up</a>
-        </div>
-      </div>
-
-      <div id="signupModal" className="modal">
-        <div className="modal-content">
-          <span className="close" onClick="closeModal('signupModal')">&times;</span>
-          <h2>Sign-up</h2>
-          <form>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" />
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" />
-            <button type="submit">Sign-up</button>
-            <a href="#">Sign up with Google</a>
-          </form>
-        </div>
-      </div>
-
-      <button onClick="openModal('loginModal')">Log In</button>
-
-      <div className='Images'>
+     <div className='Images'>
+     <h3>Popular destinations</h3>
        <div className='row'>
         <div className='image-container'>
-          <img src={Delft} alt="Delft" />
+           <img src={Delft} alt="Delft" />
           <p>Delft</p>
         </div>
         <div className='image-container'>
