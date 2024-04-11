a80820548c2aa0351c9373432afce7cd883a1468
Elmira821 <elmirauyghur@gmail.com>
2024-04-11 12:04:28 +0200

    scrolling

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,5 +1,6 @@
 import Logo from '/Logo.png'
 import './App.css'
+import React from 'react'
 import Delft from '/Delft.jpg'
 import Eindhoven from '/Eindhoven.jpg'
 import Maastricht from '/Maastricht.jpg'
@@ -10,64 +11,137 @@ import Amsterdam from '/Amsterdam.jpg'
 
 
 function App() {
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ top: 0, behavior: "smooth" });
+    }
+  };
+
   return (
     <>
       <div className="navbar">
-        <a href="#home">home</a>
-        <a href="#help">help</a>
-        <a href="#login">login</a>
-        <a href="#signup">sign-up</a>
+        <a href="#home" onClick={() => scrollToSection('home')}>Home</a>
+        <a href="#about" onClick={() => scrollToSection('about')}>About</a>
+        <a href="#help" onClick={() => scrollToSection('help')}>Help</a>
+        <a href="#login" onClick={() => scrollToSection('login')}>Login</a>
+        <a href="#signup" onClick={() => scrollToSection('login')}>Sign-up</a>
       </div>
+      <section id="home"></section>
+
       <div className="container">
         <a href="/index.html" target="_blank">
           <img src={Logo} className="logo" alt="EasyBook logo" />
         </a>
         <h1 className="title">The Hotel Booking App</h1>
       </div>
-      <div className="search-bar">
-        <div className="search-inputs">
-          <input type="text" className="search-input" placeholder="Search for hotels or destinations" />
-          <input type="text" className="date-input" placeholder="Check-in" />
-          <input type="text" className="date-input" placeholder="Check-out" />
-          <input type="number" className="guests-input" placeholder="Guests" />
-          <button className="search-button">Search</button>
-        </div>
-      </div>
-      <p className="read-the-docs">
-        Click on the Search button to look for a hotel
-      </p>
 
-     <div className='Images'>
-     <h3>Popular destinations</h3>
-       <div className='row'>
-        <div className='image-container'>
-           <img src={Delft} alt="Delft" />
-          <p>Delft</p>
+      <section className='availability'>
+
+        <form action=''>
+
+          <div className="box">
+            <p> check in <span>*</span></p>
+            <input type="date" className='input' />
+          </div>
+
+          <div className="box">
+            <p> check out <span>*</span></p>
+            <input type="date" className='input' />
+          </div>
+
+          <div className="box">
+            <p> adults <span>*</span></p>
+            <select name="adults" id='' className='input'>
+              <option value="1">1 adults</option>
+              <option value="2">2 adults</option>
+              <option value="3">3 adults</option>
+              <option value="4">4 adults</option>
+              <option value="5">5 adults</option>
+              <option value="6">6 adults</option>
+            </select>
+          </div>
+
+          <div className="box">
+            <p> children <span>*</span></p>
+            <select name="children" id='' className='input'>
+              <option value="1">1 child</option>
+              <option value="2">2 children</option>
+              <option value="3">3 children</option>
+              <option value="4">4 children</option>
+              <option value="5">5 children</option>
+              <option value="6">6 children</option>
+            </select>
+          </div>
+
+          <div className="box">
+            <p> rooms <span>*</span></p>
+            <select name="rooms" id='' className='input'>
+              <option value="1">1 room</option>
+              <option value="2">2 rooms</option>
+              <option value="3">3 rooms</option>
+              <option value="4">4 rooms</option>
+              <option value="5">5 rooms</option>
+              <option value="6">6 rooms</option>
+            </select>
+          </div>
+          <button className="search-button">Search</button>
+        </form>
+      </section>
+
+      <div className='Images'>
+        <h3>Explore Netherlands</h3>
+        <div className='row'>
+          <div className='image-container'>
+            <img src={Amsterdam} alt="Amsterdam" />
+            <p>Amsterdam</p>
+
+          </div>
+          <div className='image-container'>
+            <img src={Eindhoven} alt="Eindhoven" />
+            <p>Eindhoven</p>
+          </div>
+          <div className='image-container'>
+            <img src={Rotterdam} alt="Rotterdam" />
+            <p>Rotterdam</p>
+          </div>
         </div>
-        <div className='image-container'>
-          <img src={Eindhoven} alt="Eindhoven" />
-          <p>Eindhoven</p>
-        </div>
-        <div className='image-container'>
-          <img src={Rotterdam} alt="Rotterdam" />
-          <p>Rotterdam</p>
-        </div>
-      </div>
         <div className='row'>
           <div className='image-container'>
             <img src={DenHaag} alt="The Hague" />
             <p>The Hague</p>
           </div>
-        <div className='image-container'>
-          <img src={Maastricht} alt="Maastricht" />
-          <p>Maastricht</p>
-        </div>
-        <div className='image-container'>
-          <img src={Amsterdam} alt="Amsterdam" />
-          <p>Amsterdam</p>
+          <div className='image-container'>
+            <img src={Maastricht} alt="Maastricht" />
+            <p>Maastricht</p>
+          </div>
+          <div className='image-container'>
+            <img src={Delft} alt="Delft" />
+            <p>Delft</p>
+          </div>
         </div>
       </div>
-    </div>
+
+
+      <section className='about' id='about'>
+        <div className="row">
+          <div className="content">
+            <h3>About Us</h3>
+            <p>At EasyBook, we believe in making travel effortless and enjoyable. Whether you are planning a relaxing getaway, a business trip, or an adventure-filled holiday, we are here to simplify the process of finding the perfect accommodation for your needs.</p>
+            <p>Our mission is to provide seamless booking experiences, offering a wide range of hotels, resorts, and other accommodations worldwide. We strive to make travel planning as easy as possible, so you can focus on creating unforgettable memories.</p>
+          </div>
+        </div>
+      </section>
+
+      <section className='help' id='help'>
+        <div className="row">
+          <div className="content">
+            <h3>Welcome to the Help Center</h3>
+            <p>We are available 24 hours a day</p>
+            <p>Contact us at <span className="phone-number">060 542369</span></p>
+          </div>
+        </div>
+      </section>
 
     </>
   )
